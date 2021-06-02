a0105c9a31171f330d1d6d220eb0e49e2ef67a9d
oh-hi-yo <qaz80018@gmail.com>
2021-06-02 15:14:22 +0800

    revised function and README

--- npy_sample.py
+++ npy_sample.py
@@ -16,7 +16,7 @@ T3    ...   ...        ...
 
 #%%
 # 生成隨機table作為範例
-value = np.zeros((8,3))
+# value = np.zeros((8,3))
 
 # 舉一個例子，取出V(T1)，也就是取出第三列第零行的元素
 # v_t1 = value[3,0]
@@ -38,6 +38,7 @@ S3 = 4
 S4 = 5
 T2 = 6
 T3 = 7
+null_node = 8 # for S3
 
 s_value = 0 # state_value
 q_value_a1 = 1 # Q-value-a1
@@ -50,61 +51,189 @@ left = 0
 right = 1
 
 # (from, to) : reward
+
 reward = {(S0 , S1) : 15, (S0, S2) : 7, (S1, T1) : 7, (S1, S3) : -15, (S2, S3) : -20, 
-          (S2, S4) : 5, (S3, T2) : 30, (S4, T2) : 0, (S4, T3) : 10}
-
-
+          (S2, S4) : 5, (S3, null_node) : 0, (S3, T2) : 30, (S4, T2) : 0, (S4, T3) : 10}
 
 
 #%%
 
-check_list = [S4, S3, S2, S1, S0]
-temp = []
-left_reward = 0
-right_reward = 0
-for key, value in reward.items():
-    if key[0] == check_list[0]:
-        temp.append(key)
+def calculate_S_Q_table(probability):
+    value = np.zeros((8,3))
+
+
+    check_list = [S4, S3, S2, S1, S0]
+    
+    each_agent_prob = [] # [S4, S3, S2, S1, S0]
+    for prob in probability:
+        each_agent_prob.append([prob, 1-prob]) 
+        
+    
+    for i in range(len(check_list)):
+        temp = []
+        
+        prob_action1 = each_agent_prob[i][0]
+        prob_action2 = each_agent_prob[i][1]
+        
+        for key, val in reward.items():
+            if key[0] == check_list[i]:
+                temp.append(key)
+            
+        from_node = temp[0][0]
+        to1_node = temp[0][1]
+        to2_node = temp[1][1]
+        
+        left_reward = reward[(from_node, to1_node)]
+        right_reward = reward[(from_node, to2_node)]
+        
+    
+        if from_node == S4:
+            from_node -= 1
+            
+            # state value
+            left_value = prob_action1 * (action1_transform_prob[from_node][left] * (left_reward + discount_factor * value[to1_node, s_value]) +
+                            action1_transform_prob[from_node][right] * (right_reward + discount_factor * value[to2_node, s_value]))
+            
+            right_value = prob_action2 * (action2_transform_prob[from_node][left] * (left_reward + discount_factor * value[to1_node, s_value]) +
+                            action2_transform_prob[from_node][right] * (right_reward + discount_factor * value[to2_node, s_value]))
+            
+            value[from_node + 1, s_value] = left_value + right_value
+            
+            # q_value_a1
+            left_value = action1_transform_prob[from_node][left] * (left_reward + discount_factor * 
+                        (prob_action1 * value[to1_node, q_value_a1] + prob_action2 * value[to1_node, q_value_a2]))
+             
+            
+            right_value = action1_transform_prob[from_node][right] * (right_reward + discount_factor * 
+                        (prob_action1 * value[to2_node, q_value_a1] + prob_action2 * value[to2_node, q_value_a2]))
+            
+            value[from_node + 1, q_value_a1] = left_value + right_value
+            
+            
+            # q_value_a2
+            left_value = action2_transform_prob[from_node][left] * (left_reward + discount_factor * 
+                        (prob_action1 * value[to1_node, q_value_a1] + prob_action2 * value[to1_node, q_value_a2]))
+             
+            right_value = action2_transform_prob[from_node][right] * (right_reward + discount_factor * 
+                        (prob_action1 * value[to2_node, q_value_a1] + prob_action2 * value[to2_node, q_value_a2]))
+            
+            value[from_node + 1, q_value_a2] = left_value + right_value
+    
+        
+        elif from_node == S3:
+            from_node -= 1
+            
+            # state value
+            left_value = prob_action1 * (action1_transform_prob[from_node][left] * (left_reward + discount_factor * 0) +
+                            action1_transform_prob[from_node][right] * (right_reward + discount_factor * value[to2_node, s_value]))
+            
+            right_value = prob_action2 * (action2_transform_prob[from_node][left] * (left_reward + discount_factor * 0) +
+                            action2_transform_prob[from_node][right] * (right_reward + discount_factor * value[to2_node, s_value]))
+            
+            value[from_node + 1, s_value] = left_value + right_value
+            
+            # q_value_a1
+            left_value = action1_transform_prob[from_node][left] * (left_reward + discount_factor * 
+                        (prob_action1 * 0 + prob_action2 * 0))
+             
+            
+            right_value = action1_transform_prob[from_node][right] * (right_reward + discount_factor * 
+                        (prob_action1 * value[to2_node, q_value_a1] + prob_action2 * value[to2_node, q_value_a2]))
+            
+            value[from_node + 1, q_value_a1] = left_value + right_value
+            
+            # q_value_a2
+            left_value = action2_transform_prob[from_node][left] * (left_reward + discount_factor * 
+                        (prob_action1 * 0 + prob_action2 * 0))
+             
+            right_value = action2_transform_prob[from_node][right] * (right_reward + discount_factor * 
+                        (prob_action1 * value[to2_node, q_value_a1] + prob_action2 * value[to2_node, q_value_a2]))
+            
+            value[from_node + 1, q_value_a2] = left_value + right_value
+        
+        else:
+            # state value
+            left_value = prob_action1 * (action1_transform_prob[from_node][left] * (left_reward + discount_factor * value[to1_node, s_value]) +
+                            action1_transform_prob[from_node][right] * (right_reward + discount_factor * value[to2_node, s_value]))
+            
+            right_value = prob_action2 * (action2_transform_prob[from_node][left] * (left_reward + discount_factor * value[to1_node, s_value]) +
+                            action2_transform_prob[from_node][right] * (right_reward + discount_factor * value[to2_node, s_value]))
+            
+            value[from_node, s_value] = left_value + right_value
+            
+            # q_value_a1
+            left_value = action1_transform_prob[from_node][left] * (left_reward + discount_factor * 
+                        (prob_action1 * value[to1_node, q_value_a1] + prob_action2 * value[to1_node, q_value_a2]))
+             
+            
+            right_value = action1_transform_prob[from_node][right] * (right_reward + discount_factor * 
+                        (prob_action1 * value[to2_node, q_value_a1] + prob_action2 * value[to2_node, q_value_a2]))
+            
+            value[from_node, q_value_a1] = left_value + right_value
+            
+             # q_value_a2
+            left_value = action2_transform_prob[from_node][left] * (left_reward + discount_factor * 
+                        (prob_action1 * value[to1_node, q_value_a1] + prob_action2 * value[to1_node, q_value_a2]))
+             
+            right_value = action2_transform_prob[from_node][right] * (right_reward + discount_factor * 
+                        (prob_action1 * value[to2_node, q_value_a1] + prob_action2 * value[to2_node, q_value_a2]))
+            
+            value[from_node, q_value_a2] = left_value + right_value
+    
+    return value
+
+uniform_policy = [0.5, 0.5, 0.5, 0.5, 0.5]
+S_Q_table = calculate_S_Q_table(uniform_policy)
+
+print('value_a' ,S_Q_table)
+# 儲存根據(a)提供的policy計算出的value的array
+np.save('value_a.npy',S_Q_table)
+
+#%%
+
+
 
     
-left_reward = reward[(S4, T2)]
-right_reward = reward[(S4, T3)]
+# left_reward = reward[(S4, T2)]
+# right_reward = reward[(S4, T3)]
 
-# state value
-left_value = prob_action1 * (action1_transform_prob[S4 -1][left] * (left_reward + discount_factor * value[T2, s_value]) +
-                action1_transform_prob[S4 - 1][right] * (right_reward + discount_factor * value[T3, s_value]))
+# # state value
+# left_value = prob_action1 * (action1_transform_prob[S4 -1][left] * (left_reward + discount_factor * value[T2, s_value]) +
+#                 action1_transform_prob[S4 - 1][right] * (right_reward + discount_factor * value[T3, s_value]))
 
-right_value = prob_action2 * (action2_transform_prob[S4 - 1][left] * (left_reward + discount_factor * value[T2, s_value]) +
-                action2_transform_prob[S4 - 1][right] * (right_reward + discount_factor * value[T3, s_value]))
+# right_value = prob_action2 * (action2_transform_prob[S4 - 1][left] * (left_reward + discount_factor * value[T2, s_value]) +
+#                 action2_transform_prob[S4 - 1][right] * (right_reward + discount_factor * value[T3, s_value]))
 
-value[S4, s_value] = left_value + right_value
+# value[S4, s_value] = left_value + right_value
 
 
 # q_value_a1
-left_value = action1_transform_prob[S4 -1][left] * (left_reward + discount_factor * 
-            (prob_action1 * value[T2, q_value_a1] + prob_action2 * value[T2, q_value_a2]))
+# left_value = action1_transform_prob[S4 -1][left] * (left_reward + discount_factor * 
+#             (prob_action1 * value[T2, q_value_a1] + prob_action2 * value[T2, q_value_a2]))
  
 
-right_value = action1_transform_prob[S4 -1][right] * (right_reward + discount_factor * 
-            (prob_action1 * value[T3, q_value_a1] + prob_action2 * value[T3, q_value_a2]))
+# right_value = action1_transform_prob[S4 -1][right] * (right_reward + discount_factor * 
+#             (prob_action1 * value[T3, q_value_a1] + prob_action2 * value[T3, q_value_a2]))
 
-value[S4, q_value_a1] = left_value + right_value
+# value[S4, q_value_a1] = left_value + right_value
 
 
-# q_value_a2
-left_value = action2_transform_prob[S4 -1][left] * (left_reward + discount_factor * 
-            (prob_action1 * value[T2, q_value_a1] + prob_action2 * value[T2, q_value_a2]))
+# # q_value_a2
+# left_value = action2_transform_prob[S4 -1][left] * (left_reward + discount_factor * 
+#             (prob_action1 * value[T2, q_value_a1] + prob_action2 * value[T2, q_value_a2]))
  
 
-right_value = action2_transform_prob[S4 -1][right] * (right_reward + discount_factor * 
-            (prob_action1 * value[T3, q_value_a1] + prob_action2 * value[T3, q_value_a2]))
+# right_value = action2_transform_prob[S4 -1][right] * (right_reward + discount_factor * 
+#             (prob_action1 * value[T3, q_value_a1] + prob_action2 * value[T3, q_value_a2]))
 
-value[S4, q_value_a2] = left_value + right_value
+# value[S4, q_value_a2] = left_value + right_value
 
 #%%
 
-
-
-# 儲存根據(a)提供的policy計算出的value的array
-np.save('value_a.npy',value)
+policy_Pi = [0, 0, 1, 0, 1] # [S0, S1, S2, S3, S4]
+policy_Pi.reverse() # because check_list is [S4, S3, S2, S1, S0]
+S_Q_table = calculate_S_Q_table(policy_Pi)
+print('value_b' ,S_Q_table)
+# 儲存根據(b)提供的policy計算出的value的array
+np.save('value_b.npy', S_Q_table)
 
